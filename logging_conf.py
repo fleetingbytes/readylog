5b9dcb7d5e26c55aabe301752a506b209b93b8e4
Nagidal <nagidal146@gmail.com>
2019-06-30 14:03:44 +0200

    Look done.

--- logging_conf.py
+++ logging_conf.py
@@ -12,7 +12,7 @@ file_formatter_conf = {
 }
 
 console_formatter_conf = {
-    "format": "{asctime}.{msecs:03.0f} {levelname:>9s} {module} {funcName}: {message}",
+    "format": "{asctime},{msecs:03.0f} {levelname:>9s} {module} {funcName}: {message}",
     "style": "{",
     "datefmt": "%a %H:%M:%S",
 }
