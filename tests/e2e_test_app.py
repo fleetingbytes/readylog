8151146306b9337b7de7ef1eba91070c2b8c6ee6
fleetingbytes <7075397+fleetingbytes@users.noreply.github.com>
2025-10-28 05:57:14 +0100

    test: introduce end-to-end app test

---
+++ tests/e2e_test_app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env -S uv run --script
+#
+# /// script
+# requires-python = ">=3.11"
+# dependencies = [
+#     "readylog",
+# ]
+#
+# [tool.uv.sources]
+# readylog = { path = "..", editable = true }
+# ///
+
+from logging import getLogger
+from logging.config import dictConfig
+
+from readylog import create_dict_config
+from readylog.decorators import debug
+
+logging_configuration = create_dict_config(
+    "e2e-test-logfile.log", "e2e-test-app", console_log_level="DEBUG"
+)
+dictConfig(logging_configuration)
+
+logger = getLogger(__name__)
+
+
+@debug
+def greet(greeting: str, name: str) -> None:
+    return f"{greeting.title()}, {name.title()}"
+
+
+logger.debug("e2e-test-app runs")
+
+greet("hello", "world")
